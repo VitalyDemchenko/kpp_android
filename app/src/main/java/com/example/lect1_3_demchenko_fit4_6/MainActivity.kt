3c5e05cd3c5e97e0e8d21b0bae2c226666e6aced
VitalyDemchenko <v.demchenko.ns@gmail.com>
2023-10-03 14:28:16 +0300

    ПЕРШИЙ КОМІТ

---
+++ app/src/main/java/com/example/lect1_3_demchenko_fit4_6/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.lect1_3_demchenko_fit4_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lect1-3_demchenko_fit4-6"
+include(":app")
